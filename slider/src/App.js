b28a40727dcc1cf178419f31bea6cb0be0ad9268
Andrey <aguttuev@gmail.com>
2018-07-08 20:51:37 +0600

    Это второй коммит

--- slider/src/App.js
+++ slider/src/App.js
@@ -2,29 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
 
+const url = process.env.PUBLIC_URL + './img/bg.png';
+
+const HeaderWrapper = styled.header`
+width: 100%;
+height: 49px;
+background-color: #242424;
+opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+height: 89px;
+padding-top: 21px;
+`
+const MainWrapper = styled.main`
+height: 700px;
+padding-top: 130px;
+background: url(${url}) no-repeat;
+background-size: cover;
+`
 class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-      <SayFullName name="Andrey" family="Gyuttuev" link="google.com" />
-      <SayFullName name="Andrey" family="Ivanov" link="ya.ru" />
-      <SayFullName name="Andrey" family="Petrov" link="vk.com" />
-      </div>
-    );
+  render(){
+    return(
+    <div className="App">
+      <HeaderWrapper>
+        <Grid>
+          <Header />
+        </Grid>
+      </HeaderWrapper>
+      <MenuWrapper>
+        <Grid>
+          <Menu />
+        </Grid>
+      </MenuWrapper>
+      <MainWrapper>
+        <Grid>
+          <Main />
+        </Grid>
+      </MainWrapper>
+  </div>
+      )
   }
+  
 }
 
-function SayFullName(props) {
-  return (
-<div>   
-<h1>мое имя {props.name}, фамилия - {props.family}</h1>
-<a href={props.link}> Ссылка на мой профиль </a>
-</div>
-    )
-}
+
 
 export default App;
